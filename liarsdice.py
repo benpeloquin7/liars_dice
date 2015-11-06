f0b5195b4b906f51a9b960cec825a7bae7f1959b
Jason Freeman <jfreeman@microsoft.com>
2015-11-06 12:36:12 -0800

    GameState classes for initial and medial game states

--- liarsdice.py
+++ liarsdice.py
@@ -6,7 +6,10 @@ Terminology:
 """
 
 import random
+from collections import Counter
 
+NUM_PLAYERS = 3
+DICE_SIDES = 6
 class PlayerRules:
     """
     Represents the rules of the game from players' perspective.
@@ -51,143 +54,120 @@ class PlayerRules:
 
     applyAction = staticmethod( applyAction )
 
-
-class CentralControl():
-    """
-    Currently stores functions that update the gamestate based on player actions.
-
-    May want to merge this with PlayerRules, but the idea is CentralControl is more like "nature", i.e.,
-    this class is "allowed" to access gameState.trueHands.
-    """
-    def __init__(self):
-        #ISSUE: Note sure if this should have a constructor, or just be staticmethods like PlayerRules.
-        pass
-
-    def evaluateAction(self, state, action ):
-        """
-        Refers to the true hands in gameState to resolve callBluffs and confirmBids
-        """
-        trueHands = state.trueHands
-        cp = state.data.currentPlayer #current player
-        pp = state.data.previousPlayer
-
-        (bidQuant, bidVal) = state.currBid
-
-        s = 0
-        #Compute quantity of bidVal that really exist
-        for hand in trueHands:
-            for val in hand:
-                s+= (val == bidVal)
-
-        if action == 'callBluff':
-            if bidQuant >= s:
-                #cp loses a die
-                state.dicePerPlayer[cp] -= 1
-            else:
-                #pp loses a die
-                state.dicePerPlayer[pp] -= 1
-
-        elif action == 'confirmBid':
-            if bidQuant != s:
-                state.dicePerPlayer[cp] -= 1
-            else:
-                #Everyone loses a die except cp.
-                state.dicePerPlayer = [dice-1 for ix, dice in enumerate(state.dicePerPlayer) if ix != cp]
-
-
-    def reroll(self, state):
-        """
-        Rerolls everyone's dice, setting a new .trueHands in the GameState.
-
-        Note that reroll should be called iff dicePerPlayer changes, as in evaluateAction.
-        """
-        state.trueHands = []
-        dpp = state.dicePerPlayer
-
-        for i in range(state.numOpps+1):
-            state.trueHands.append([random.randint(1,6) for j in range(0,dpp[i])])
-
-
-class GameStateData:
-    """
-    Pacman starter code had a class for GameStateData (game.py) and GameState (pacman.py). Following the same convention,
-    GameStateData contains all the data about the GameState, where GameState includes methods for accessing the data.
-
-    Should consider merging it all into one class.
-    """
-    def __init__( self, prevState = None ):
-        """
-        Generates a new data packet by copying information from its predecessor.
-        """
-        if prevState != None:
-            self.trueHands = prevState.trueHands.deepCopy()
-            self.dicePerPlayer = prevState.dicePerPlayer
-            self.numOpps = prevState.numOpps
-            self.currBid = prevState.currBid.deepCopy()
-
-            #ISSUE: Obviously these need to incremement. But where should that happen?
-            self.currPlayer = prevState.currPlayer
-            self.prevPlayer = prevState.prevPlayer
-
-        self.trueHands = []
-        self.dicePerPlayer = None
-        self.numOpps = None
-        self._lose = False
-        self._win = False
-
-    def initialize( self, dicePerPlayer, diceSides, numOpps ):
-        """
-        Initialize the game.
-        """
-        self.totalDice = dicePerPlayer*numOpps
-
-        self.trueHands = [] #List of sub-lists, where each sub-list holds a different player's dice values. 0th sub-list is agent's.
-        self.diceSides = diceSides #Number of sides per dice. Typically 6.
-        self.dicePerPlayer = [dicePerPlayer]*(numOpps + 1) #List keeping track of how many dice each player has.
-        self.numOpps = numOpps
-
-        for i in range(numOpps+1):
-            self.trueHands.append([random.randint(1,6) for j in range(0,dicePerPlayer[i])])
-
-        self.currPlayer = 0 #ISSUE: Always give our agent the first turn?
-        self.currBid = (None, None)
-        self.prevPlayer = None
-
-
 class GameState:
     def __init__(self):
+        pass
+
+    def getLegalActions(self):
+        raise Exception("abstract")
+
+    def generateSuccessor(self):
+        raise Exception("abstract")
+
+    def isWin(self, playerIndex):
+        return False
+
+    def isLose(self, playerIndex):
+        return False
+
+    def getNextPlayer(self):
+        nextPlayer = (self.currentPlayerIndex + 1) % NUM_PLAYERS
+        # skip players that are out
+        while self.numDicePerPlayer[nextPlayer] == 0:
+            nextPlayer = (self.currentPlayerIndex + 1) % NUM_PLAYERS
+
+        return nextPlayer
+
+
+class InitialGameState(GameState):
+    def __init__(self, numDicePerPlayer, currentPlayerIndex):
         """
         Generates a new state by copying information from its predecessor.
         """
-        if prevState != None: # Initial state
-            self.data = GameStateData(prevState.data)
-        else:
-            self.data = GameStateData()
+        self.hands = [Counter(random.randint(1,DICE_SIDES) for _ in range(numDice)) for numDice in numDicePerPlayer]
+        self.bid = None
+        self.currentPlayerIndex = currentPlayerIndex
+        self.numDicePerPlayer = numDicePerPlayer
 
-
-    def initialize( self, dicePerPlayer, diceSides, numOpps ):
-        """
-        Creates an initial game state.
-        """
-        self.data.initialize( self, dicePerPlayer, diceSides, numOpps )
-
-    def getLegalActions( self, playerIndex=0 ):
+    def getLegalActions(self):
         """
         Returns the legal actions for the player specified.
         """
-        if self.isWin() or self.isLose(): return []
+        totalNumberOfDice = sum(self.numDicePerPlayer)
+        return [('bid', value, count, self.currentPlayerIndex)
+                for value in range(1, DICE_SIDES + 1) for count in range(1, totalNumberOfDice + 1)]
 
-        return PlayerRules.getLegalActions(self)
-
-    def generateSuccessor( self, playerIndex, action):
+    def generateSuccessor(self, action):
         """
         Returns the successor state after the specified player takes the action.
         """
-        # Check that successors exist
-        if self.isWin() or self.isLose(): raise Exception('Can\'t generate a successor of a terminal state.')
+        verb, value, count, bidPlayer = action
+        assert verb == 'bid'
 
-        # Copy current state
-        state = GameState(self)
+        return MedialGameState(self.numDicePerPlayer, self.hands, self.getNextPlayer(), action)
 
-        PlayerRules.applyAction( state, action, playerIndex)
-        return state
+    def isLose(self, playerIndex):
+        return self.numDicePerPlayer[playerIndex] == 0
+
+    def isWin(self, playerIndex):
+        return self.numDicePerPlayer[playerIndex] > 0 and\
+               all(numDice == 0 for otherPlayer, numDice in enumerate(self.numDicePerPlayer) if otherPlayer != playerIndex)
+
+class MedialGameState(GameState):
+    def  __init__(self, numDicePerPlayer, hands, currentPlayerIndex, bid):
+        self.hands = hands
+        self.currentPlayerIndex = currentPlayerIndex
+        self.bid = bid
+        self.numDicePerPlayer = numDicePerPlayer
+
+    def getLegalActions(self):
+        """
+        Returns the legal actions for the player specified.
+        """
+        currentCount = self.bid[2]
+        totalNumberOfDice = sum(sum(hand.values()) for hand in self.hands)
+        actions = [('bid', value, count, self.currentPlayerIndex)
+                for value in range(1, DICE_SIDES + 1) for count in range(currentCount + 1, totalNumberOfDice + 1)]
+
+        actions.append(('confirm', self.bid[1], self.bid[2], self.bid[3]))
+        actions.append(('deny', self.bid[1], self.bid[2], self.bid[3]))
+
+    def generateSuccessor(self, action):
+        """
+        Returns the successor state after the specified player takes the action.
+        """
+        verb, value, count, bidPlayerIndex = action
+        if verb == 'bid':
+
+            return MedialGameState(self.numDicePerPlayer,
+                                   self.hands,
+                                   self.getNextPlayer(),
+                                   action)
+        else:
+            # copy numDicePerPlayer
+            numDicePerPlayer = [numDice for numDice in self.numDicePerPlayer]
+            trueCountOfReleventDie = sum(hand[value] for hand in self.hands)
+
+            if verb == 'confirm':
+                if count == trueCountOfReleventDie:
+                    for playerIndex, numDice in enumerate(numDicePerPlayer):
+                        if playerIndex != self.currentPlayerIndex and numDice > 0:
+                            # each player loses a die, except the confirming player
+                            numDicePerPlayer[playerIndex] -= 1
+                            nextPlayer = self.currentPlayerIndex
+                else:
+                    # confirming player loses a die
+                    numDicePerPlayer[self.currentPlayerIndex] -= 1
+                    nextPlayer = bidPlayerIndex
+            else:
+                assert verb == 'deny'
+                if count < trueCountOfReleventDie:
+                    # prev player loses a die
+                    numDicePerPlayer[bidPlayerIndex] -= 1
+                    nextPlayer = self.currentPlayerIndex
+                else:
+                    # denying player loses a die, same as confirming
+                    numDicePerPlayer[self.currentPlayerIndex] -= 1
+                    nextPlayer = self.currentPlayerIndex
+
+            return InitialGameState(numDicePerPlayer, nextPlayer)
