5c2307342d52c9b91f40c1e101b46abce5f648b8
Jason Freeman <jfreeman@microsoft.com>
2015-11-07 17:47:28 -0800

    More on human agent input

--- agents.py
+++ agents.py
@@ -9,12 +9,25 @@ class HumanAgent:
         self.agentIndex = agentIndex
 
     def chooseAction(self, gameState):
+        def validateActionFormat(actionParts):
+            if len(actionParts) != 3:
+                return False
+            if actionParts[0] not in ('bid', 'confirm', 'deny'):
+                return False
+            if not actionParts[1].isdigit() or not actionParts[2].isdigit():
+                return False
+            return True
+
         possibleActions = gameState.getLegalActions()
-        actionString = input('Player %d, Enter your action: ' % self.agentIndex)
+        actionString = raw_input('Player %d, Enter your action: ' % self.agentIndex)
         while True:
             # parse the action
-            action = tuple(actionString.split()) + (self.agentIndex, )
-            if action in possibleActions:
-                return action
+            actionParts = actionString.split()
+            if validateActionFormat(actionParts):
+                verb, value, count = actionParts
+                action = verb, int(value), int(count), self.agentIndex
 
-            actionString = input('That is not a legal action. Player %d, Try again: ' % self.agentIndex)
+                if action in possibleActions:
+                    return action
+
+            actionString = raw_input('That is not a legal action. Player %d, Try again: ' % self.agentIndex)
