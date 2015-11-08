94e901fee74882f59dac6e33cbf32c937e5f56d5
Jason Freeman <jfreeman@microsoft.com>
2015-11-07 19:32:51 -0800

    Make UI nicer

--- agents.py
+++ agents.py
@@ -10,11 +10,14 @@ class HumanAgent:
 
     def chooseAction(self, gameState):
         def validateActionFormat(actionParts):
-            if len(actionParts) != 3:
-                return False
             if actionParts[0] not in ('bid', 'confirm', 'deny'):
                 return False
-            if not actionParts[1].isdigit() or not actionParts[2].isdigit():
+            if actionParts[0] == 'bid':
+                if len(actionParts) != 3:
+                    return False
+                if not actionParts[1].isdigit() or not actionParts[2].isdigit():
+                    return False
+            elif len(actionParts) != 1: #confirm or deny
                 return False
             return True
 
@@ -24,10 +27,14 @@ class HumanAgent:
             # parse the action
             actionParts = actionString.split()
             if validateActionFormat(actionParts):
-                verb, value, count = actionParts
-                action = verb, int(value), int(count), self.agentIndex
+                if actionParts[0] == 'bid':
+                    verb, count, value = actionParts
+                    action = verb, int(value), int(count), self.agentIndex
+                    if action in possibleActions:
+                        return action
+                else: # confirm or deny
+                    matchingActions = filter(lambda a: a[0] == actionParts[0], possibleActions)
+                    if len(matchingActions) > 0:
+                        return matchingActions[0]
 
-                if action in possibleActions:
-                    return action
-
-            actionString = raw_input('That is not a legal action. Player %d, Try again: ' % self.agentIndex)
+            actionString = raw_input('That is not a legal action.\nPlayer %d, Try again: ' % self.agentIndex)
