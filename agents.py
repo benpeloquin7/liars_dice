f9bf6c89b367045987bc21c4d54138c41d080da2
Alex Kuefler <kuefleralex@gmail.com>
2015-11-08 19:19:23 -0800

    agents.py

--- agents.py
+++ agents.py
@@ -1,8 +1,46 @@
 from liarsdice import *
+import random
 
 class Agent:
     def chooseAction(self, gameState):
         raise Exception('abstract')
+
+class OracleAgent:
+    def __init__(self, agentIndex):
+        self.agentIndex = agentIndex
+
+    def chooseAction(self, gameState):
+        legalActions = gameState.getLegalActions()
+        goodActions = []
+        def checkTrue(bid):
+            (bidstr, value, count, agentIndex) = bid
+            s = 0
+            for hand in gameState.hands:
+                s += hand[value]
+
+            if s > count:
+                return 1
+            elif s == count:
+                return 2
+            else:
+                return 0
+
+        #Pick a list of true bids to make.
+        for act in legalActions:
+            if(checkTrue(act) > 0):
+                goodActions.append(act)
+
+        if gameState.bid is not None:
+            bid = gameState.bid
+            if checkTrue(gameState.bid) == 0:
+                return ('deny', bid[1], bid[2], bid[3])
+            elif checkTrue(gameState.bid) == 2:
+                return ('confirm', bid[1], bid[2], bid[3])
+        else:
+            ix = random.randint(0, len(goodActions)-1)
+            return goodActions[ix]
+
+        pass
 
 class HumanAgent(Agent):
     def __init__(self, agentIndex):
